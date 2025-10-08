7c17a83d803663d6760436fd0103443c372824d7
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-08 21:29:52 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld2/HelloWorld.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _HelloWorld : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var todayDate = DateTime.Now;
+        Response.Clear();
+        Response.Write("<h1>Hello World</h1>");
+        Response.Write("ASP.Net Page<br>");
+        Response.Write("Today is:" + todayDate+"<br>");
+        Response.Write("<a href=\"Index.html\">Simple web html page</a><br>");
+        Response.Write("<a href=\"HelloWorld.ashx\">Simple Web Handler Module</a><br>");
+    }
+}
